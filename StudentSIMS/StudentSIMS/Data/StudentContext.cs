13b5660a955d7c29cb74dc2f7e65e6a4ac0fa29a
raymondchau98 <raymond.chau98@gmail.com>
2020-06-26 22:27:50 +1200

    Add new project with CORS

---
+++ StudentSIMS/StudentSIMS/Data/StudentContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using StudentSIMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSIMS.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext()
+        {
+
+        }
+        public StudentContext(DbContextOptions<StudentContext> options)
+        : base(options)
+        {
+
+        }
+        public DbSet<Student> Student { get; set; }
+        public static System.Collections.Specialized.NameValueCollection AppSettings { get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+           .AddJsonFile("appsettings.json")
+           .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("schoolSIMSConnection"));
+        }
+    
+    
+    }
+}
